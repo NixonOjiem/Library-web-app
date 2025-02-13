811bacfb7b352bc4127c5c35fd6fbe0d6a817d37
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-13 08:15:46 +0300

    added more pictures

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from 'react';
 import banner1 from '../assets/images/banner1.jpg';
 import banner2 from '../assets/images/banner2.jpg';
 import banner3 from '../assets/images/banner3.jpg';
+import banner4 from '../assets/images/banner4.jpg';
+import banner5 from '../assets/images/banner5.jpg';
+import banner6 from '../assets/images/banner6.jpg';
+import banner7 from '../assets/images/banner7.jpg';
+import banner8 from '../assets/images/banner8.jpg';
 
-const images = [banner1, banner2, banner3];
+const images = [banner1, banner2, banner3, banner4, banner5, banner6, banner7, banner8];
 
 function Banner() {
   const [currentIndex, setCurrentIndex] = useState(0);
