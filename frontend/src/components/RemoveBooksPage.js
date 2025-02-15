5fcf964a40f4dd62723953fd5229aaf81f49ef72
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-15 12:21:36 +0300

    started working on add a book and remove a book

--- frontend/src/components/RemoveBooksPage.js
+++ frontend/src/components/RemoveBooksPage.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function RemoveBooksPage() {
+  const [showAddBooks, setShowAddBooks] = useState(true);
+
+  const handleRemoveBooksPage = () => {
+    setShowAddBooks(false);
+  }
+  const handleAddBooksPage = () => {
+    setShowAddBooks(true);
+  }
+
   return (
-    <div>RemoveBooksPage</div>
+    <div>
+      <h1>Book Management</h1>
+      {showAddBooks ? (
+        <div className='bookmanagement'>
+          <h1>Add a book page</h1>
+          <form>
+          </form>
+          <p>To remove books click here: 
+            <a className='remove-books-link' onClick={handleRemoveBooksPage}>
+             RemoveBooks
+            </a>
+          </p>
+        </div>
+      ):(
+        <div className='bookmanagement'>
+          <h1>Remove a book page</h1>
+          <form>
+          </form>
+          <p>To add books click here:
+            <a className='add-books-link' onClick={handleAddBooksPage}>
+              AddBooks
+            </a>
+          </p>
+        </div>
+      )}
+    </div>
   )
 }
 
