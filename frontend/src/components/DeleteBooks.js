460d94229e31a15d3d4604c8594b76f11c989d47
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-17 09:49:44 +0300

    created a front end end point to access book clicked on DeleteBooks Component

--- frontend/src/components/DeleteBooks.js
+++ frontend/src/components/DeleteBooks.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function DeleteBooks({ bookID, onBack }) {
+    const [book, setBook] = useState([]);
+
+    useEffect(() => {
+        const fetchBook = async () => {
+          try {
+            const response = await axios.get(`http://localhost:3001/books/${bookID}`);
+            setBook(response.data);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchBook();
+      }, [bookID]);
+      
   return (
     <div>
       <h1>Delete Books</h1>
