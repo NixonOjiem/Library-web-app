c6227327999c95f1023f1d8befac4b40b9fd4b11
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-17 11:09:53 +0300

    Added more fields in DeleteBooks Component

--- frontend/src/components/DeleteBooks.js
+++ frontend/src/components/DeleteBooks.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import admin_image from '../assets/images/adminimage1.jpg';
 
-function YourComponent({ bookID }) {
+function DeleteBooks({ bookID, onBack }) {
   const [book, setBook] = useState({}); // Initialize as an object
 
   useEffect(() => {
@@ -17,13 +18,57 @@ function YourComponent({ bookID }) {
     fetchBook();
   }, [bookID]);
 
+ const handleImageChange=()=>{
+
+ }
   return (
     <div>
       {book.title ? (
         <div>
-          <h1>{book.title}</h1>
-          <p>{book.author}</p>
-          {/* Render other book details here */}
+          <div className='admin-image'>
+            <img src={admin_image} alt='An image appears here' />
+          </div>
+          <div className='Admin-form'>
+            <form className='book-details-form'>
+                <label>
+                    <span>Title</span>
+                    <input type='text' value={book.title}  />
+                </label>
+                
+                <label>
+                    <span>Author</span>
+                    <input type='text' value={book.author} />
+                </label>
+
+                <label>
+                    <span>Year Published</span>
+                    <input type='number' value={book.publiched_date} />
+                </label>
+
+                <label>
+                    <span>Genre</span>
+                    <input type='text' value={book.genre} />
+                </label>
+
+                <label>
+                    <span>ISBN</span>
+                    <textarea value={book.isbn} />
+                </label>
+
+                <label>
+                    <span>Copies Available</span>
+                    <input type='number' value={book.copies_available} />
+                </label>
+
+                <label>
+                    <span>Book Image</span>
+                    <input type='file' onChange={handleImageChange} value={book.cover_image}/>
+                </label>
+
+            </form>
+          </div>
+          
+          <button onClick={onBack}>Back</button>
         </div>
       ) : (
         <p>Loading...</p>
@@ -32,4 +77,4 @@ function YourComponent({ bookID }) {
   );
 }
 
-export default YourComponent;
+export default DeleteBooks;
