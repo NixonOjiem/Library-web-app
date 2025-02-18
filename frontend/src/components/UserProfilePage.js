817472e0a3350592c2ad66989690b8e138b08cfa
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-18 09:47:07 +0300

    started working on the profile page

--- frontend/src/components/UserProfilePage.js
+++ frontend/src/components/UserProfilePage.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function UserProfilePage() {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem('username_local');
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []); // Empty dependency array ensures this runs only once
+
+  console.log(username);
+
   return (
-    <div>UserProfilePage</div>
-  )
+    <div>
+      Hey there, {username}
+    </div>
+  );
 }
 
-export default UserProfilePage
+export default UserProfilePage;
