b814ae87dcb4a76b659b900da4a384afc0833f3f
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-21 07:39:19 +0300

    Added a frontend API to fetch books borrowed

--- frontend/src/components/BrorrowedBooksPage.js
+++ frontend/src/components/BrorrowedBooksPage.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
 
 function BrorrowedBooksPage() {
+  const [borrowedBooks, setBorrowedBooks] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/borrowed-books');
+        console.log(response.data); // Do something with the response
+        setBorrowedBooks(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
   return (
-    <div>BrorrowedBooksPage</div>
+    <div className='borrowed-books'>
+      BrorrowedBooksPage
+    </div>
   )
 }
 
