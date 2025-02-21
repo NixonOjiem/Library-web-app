06e4076206902f7b5b9ce5ff446f45b123f4d195
arkstudiodevelopers <nicksonogm@gmail.com>
2025-02-21 10:42:01 +0300

    Added a serach function in borrowed books page

--- frontend/src/components/BrorrowedBooksPage.js
+++ frontend/src/components/BrorrowedBooksPage.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 
-function BrorrowedBooksPage() {
+function BorrowedBooksPage() {
   const [borrowedBooks, setBorrowedBooks] = useState([]);
   const [search, setSearch] = useState("");
+  const [filteredBooks, setFilteredBooks] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,6 +12,7 @@ function BrorrowedBooksPage() {
         const response = await axios.get('http://localhost:5000/borrowed-books');
         console.log(response.data); // Do something with the response
         setBorrowedBooks(response.data);
+        setFilteredBooks(response.data); // Initialize filteredBooks with all borrowedBooks
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -19,15 +21,32 @@ function BrorrowedBooksPage() {
     fetchData();
   }, []);
 
+  const handlerSearch = () => {
+    const lowercasedSearch = search.toLowerCase();
+    const filtered = borrowedBooks.filter(book => 
+      book.username.toLowerCase().includes(lowercasedSearch) || 
+      book.title.toLowerCase().includes(lowercasedSearch)
+    );
+    setFilteredBooks(filtered);
+  };
+
   return (
     <div className='borrowed-books'>
-      <h1>BrorrowedBooksPage</h1>
-      <div className='books-container'>
+      <h1>Borrowed Books Page</h1>
+      <input 
+        type='text' 
+        className='borrowed-books-search' 
+        placeholder='search for a user or book...' 
+        value={search}
+        onChange={(e) => setSearch(e.target.value)} 
+      />
+      <button className='search-borrowed-books-button' onClick={handlerSearch}>Search</button>
+      <div className='all-borrowed-books-container'>
         {
-          borrowedBooks.map((books, index) => (
-            <div key = {index}>
-              <p>User:{books.username}</p>
-              <p>Book Name:{books.title}</p>
+          filteredBooks.map((books, index) => (
+            <div key={index} className='borrowed-book-item'>
+              <p>User: {books.username}</p>
+              <p>Book Name: {books.title}</p>
               <p>Date Borrowed: {books.date_borrowed}</p>
               <p>Expected Return Date: {books.date_expected_return}</p>
             </div>
@@ -35,7 +54,7 @@ function BrorrowedBooksPage() {
         }
       </div>
     </div>
-  )
+  );
 }
 
-export default BrorrowedBooksPage
+export default BorrowedBooksPage;
